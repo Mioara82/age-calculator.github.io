fc8cf505be63bc5f8d4d1fcc6c42cf4fa015a92b
Mioara82 <128102876+Mioara82@users.noreply.github.com>
2023-06-26 21:12:54 +0100

    Age calculator app
    to design an age calculator app with active, success and error states

---
+++ script.js
@@ -0,0 +1,6 @@
+const dayInput = document.getElementById('dayInput');
+const monthInput = document.getElementById('monthInput');
+const yearInput = document.getElementById('yearInput');
+const yearsCalc = document.getElementById('year-result');
+const monthsCalc = document.getElementById('month-result');
+const daysCalc = document.getElementById('day-result');
